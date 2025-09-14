ab1b14ca417d10ea301f322709058a396e218ab4
Your Name <you@example.com>
2025-09-14 18:34:29 +0530

    Initial commit

---
+++ bag.service/src/test/java/com/bhrasta/bag/service/BagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhrasta.bag.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
